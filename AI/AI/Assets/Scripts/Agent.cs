60a8bee9c4c49bd33c17f1faa28d0616dea9e2b7
gurshane <gurshane.sidhu@mail.mcgill.ca>
2016-11-27 18:53:33 -0500

    PowerUp code and Pre-Fabs

--- AI/AI/Assets/Scripts/Agent.cs
+++ AI/AI/Assets/Scripts/Agent.cs
@@ -5,39 +5,29 @@ public class Agent : MonoBehaviour {
 
     public float maxSpeed;
     public float maxAccel;
-    public float orientation;
     public float rotation;
     public Vector3 velocity;
     protected Steering steering;
+    protected Rigidbody m_rigidBody;
     
     void Start()
     {
         velocity = Vector3.zero;
         steering = new Steering();
+        m_rigidBody = gameObject.GetComponent<Rigidbody>();
     }
 
     //uses the new velocity and rotation to change the displacement of the agent and to rotate it
-    public virtual void Update()
+    public void Update()
     {
         Vector3 displacement = velocity * Time.deltaTime;
-        orientation += rotation * Time.deltaTime;
-
-        if (orientation < 0.0f)
-        {
-            orientation += 360.0f;
-        }
-        else if(orientation > 360.0f)
-        {
-            orientation -= 360.0f;
-        }
 
         transform.Translate(displacement, Space.World);
         transform.rotation = new Quaternion();
-        transform.Rotate(Vector3.up, orientation);
     }
 
     //updates the velocity and the rotation with the agents steering behaviour
-    public virtual void LateUpdate()
+    public void LateUpdate()
     {
         velocity += steering.linear * Time.deltaTime;
         rotation += steering.angular * Time.deltaTime;
@@ -46,11 +36,6 @@ public class Agent : MonoBehaviour {
         {
             velocity.Normalize();
             velocity = velocity * maxSpeed;
-        }
-
-        if(steering.angular == 0.0f)
-        {
-            rotation = 0.0f;
         }
 
         if(steering.linear.sqrMagnitude == 0.0f)
---
+++ PowerUps/PowerUps/Assets/Scripts/SuperUp.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SuperUp : PowerUpModifier {
+
+    public override float getMaxAccelerationModifier()
+    {
+        return 10.0f;
+    }
+
+    public override float getJumpHeightModifier()
+    {
+        return 10.0f;
+    }
+
+    public override float getMaxHealthModifier()
+    {
+        return 1.0f;
+    }
+}
