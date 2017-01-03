e2daf63fbb89a223fd105a3a074323f908c029b4
gurshane <gurshane.sidhu@mail.mcgill.ca>
2017-01-03 18:11:43 -0500

    More BG stuff and a Tower piece

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    private AudioSource audioSource;
+    public AudioClip runSound;
+    public AudioClip jumpSound;
+
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+                audioSource.PlayOneShot(jumpSound);
+            
+        }
+        else if(Input.GetKey(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if(!audioSource.isPlaying)
+            {
+                audioSource.PlayOneShot(runSound);
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+
+	}
+}
