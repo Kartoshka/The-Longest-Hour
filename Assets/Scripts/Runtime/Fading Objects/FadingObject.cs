a4d324e587ba1b7dbbd67694a863733485754dab
Kartoshka <eli.harfouche@gmail.com>
2017-01-18 14:16:30 -0500

    Fade Object demo

---
+++ Assets/Scripts/Runtime/Fading Objects/FadingObject.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class FadingObject : TimeBasedObjects {
+
+
+	private Material mat;
+	private float initialFade =1;
+
+	public float duration = 0;
+	public float accumulatedTime = 0;
+
+	void Start()
+	{
+		mat = this.gameObject.GetComponent<MeshRenderer> ().material;
+		initialFade = mat.GetFloat ("_AlphaValue");
+	}
+
+	protected override void OnPause ()
+	{
+		
+	}
+
+	protected override void OnResume ()
+	{
+
+	}
+
+	protected override void PausedUpdate ()
+	{
+	
+	}
+
+	protected override void RunningUpdate ()
+	{
+		accumulatedTime += Time.deltaTime;
+		float fade = (duration - accumulatedTime) / duration;
+		mat.SetFloat ("_AlphaValue", fade);
+			
+		if (accumulatedTime >= duration) {
+			Destroy (this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Runtime/Fading Objects/TimeBasedObjects.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TimeBasedObjects : MonoBehaviour {
+
+
+	bool paused = false;
+	// Update is called once per frame
+	void Update () {
+
+		if (paused) {
+			PausedUpdate ();
+		} 
+		else {
+			RunningUpdate ();
+		}
+	}
+
+	public void Pause()
+	{
+		if (!paused) {
+			paused = true;
+			OnPause ();
+		}
+	}
+
+	public void Resume()
+	{
+		if (paused) {
+			paused = false;
+			OnResume ();
+		}
+	}
+
+	protected abstract void OnPause ();
+
+	protected abstract void OnResume();
+
+	protected abstract void PausedUpdate ();
+
+	protected abstract void RunningUpdate();
+
+
+}
