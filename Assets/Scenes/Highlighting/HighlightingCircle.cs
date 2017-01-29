63268cbc0993f72f184a33d62cb69caf8078fb2c
gurshane <gurshane.sidhu@mail.mcgill.ca>
2017-01-29 17:50:55 -0500

    highlighting from the aerial view

---
+++ Assets/Scenes/Highlighting/HighlightingCircle.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightingCircle : MonoBehaviour {
+
+    public KeyCode triggerKey;
+
+    private bool heldDown;
+    
+    void Start()
+    {
+        heldDown = false;
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(!heldDown && Input.GetKeyDown(triggerKey))
+        {
+            heldDown = true;
+            foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Enemy"))
+            {
+                obj.GetComponent<CircleTrail>().enabled = true;
+            }
+
+            foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Objective"))
+            {
+                obj.GetComponent<CircleTrail>().enabled = true;
+            }
+        }
+
+        if(Input.GetKeyUp(triggerKey))
+        {
+            heldDown = false;
+            foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Enemy"))
+            {
+                obj.GetComponent<CircleTrail>().enabled = false;
+            }
+
+            foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Objective"))
+            {
+                obj.GetComponent<CircleTrail>().enabled = false;
+            }
+        }
+	}
+}
