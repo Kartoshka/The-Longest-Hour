183b429b0efdeca71c82e3baa89840eced69e676
gurshane <gurshane.sidhu@mail.mcgill.ca>
2017-01-29 21:42:08 -0500

    Circle highlighter

---
+++ Assets/CircleTrail.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleTrail : MonoBehaviour {
+
+    public float r;
+    public float g;
+    public float b;
+    public float a;
+
+    public void Start()
+    {
+        updateColor();
+    }
+
+	public void updateColor()
+    {
+        gameObject.GetComponent<MeshRenderer>().material.SetColor("_Color", new Color(r, g, b, a));
+    }
+}
---
+++ Assets/Scenes/Highlighting/RotateMe.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMe : MonoBehaviour {
+
+    public float rotateAmount;
+    
+	void Update ()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * rotateAmount);
+	}
+
+    
+}
