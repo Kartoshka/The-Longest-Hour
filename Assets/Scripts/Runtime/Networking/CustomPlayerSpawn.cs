bc263191ac11fbaba6581848156318c0d69a0fdd
Mojar <jarvis.marc@gmail.com>
2017-01-31 16:13:03 -0500

    Networked Game WIP

---
+++ Assets/Scripts/Runtime/Networking/CustomPlayerSpawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CustomPlayerSpawn : NetworkBehaviour
+{
+
+	public override void OnStartLocalPlayer() // this is our player
+	{
+		base.OnStartLocalPlayer();
+
+		GameObject playerObj = GameObject.Instantiate(NetworkLobbyManager.singleton.spawnPrefabs[playerControllerId]); // instantiate on server side
+        NetworkServer.AddPlayerForConnection(NetworkServer.connections[0], playerObj, playerControllerId); // spawn on the clients and set owner
+	}
+}
