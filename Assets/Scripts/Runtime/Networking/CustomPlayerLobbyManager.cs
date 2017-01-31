da37d0c8aeb91a63294f5d2f1d8047f1087636f4
Mojar <jarvis.marc@gmail.com>
2017-01-31 16:18:36 -0500

    Removing Kicked-from-lobby issue.

--- Assets/Scripts/Runtime/Networking/CustomPlayerLobbyManager.cs
+++ Assets/Scripts/Runtime/Networking/CustomPlayerLobbyManager.cs
@@ -23,72 +23,72 @@ public class CustomPlayerLobbyManager : LobbyManager
 	//       NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
 	//}
 
-	public class MsgTypes
-	{
-		public const short PlayerPrefab = MsgType.Highest + 1;
+	//public class MsgTypes
+	//{
+	//	public const short PlayerPrefab = MsgType.Highest + 1;
 
-		public class PlayerPrefabMsg : MessageBase
-		{
-			public short controllerID;
-			public short prefabIndex;
-		}
-	}
+	//	public class PlayerPrefabMsg : MessageBase
+	//	{
+	//		public short controllerID;
+	//		public short prefabIndex;
+	//	}
+	//}
 
-	// in the Network Manager component, you must put your player prefabs 
-	// in the Spawn Info -> Registered Spawnable Prefabs section 
-	public short playerPrefabIndex;
+	//// in the Network Manager component, you must put your player prefabs 
+	//// in the Spawn Info -> Registered Spawnable Prefabs section 
+	//public short playerPrefabIndex;
 
 
-	public override void OnStartServer()
-	{
-		NetworkServer.RegisterHandler(MsgTypes.PlayerPrefab, OnResponsePrefab);
-		base.OnStartServer();
-	}
+	//public override void OnStartServer()
+	//{
+	//	NetworkServer.RegisterHandler(MsgTypes.PlayerPrefab, OnResponsePrefab);
+	//	base.OnStartServer();
+	//}
 
-	public override void OnClientConnect(NetworkConnection conn)
-	{
-		client.RegisterHandler(MsgTypes.PlayerPrefab, OnRequestPrefab);
-		base.OnClientConnect(conn);
-	}
+	//public override void OnClientConnect(NetworkConnection conn)
+	//{
+	//	client.RegisterHandler(MsgTypes.PlayerPrefab, OnRequestPrefab);
+	//	base.OnClientConnect(conn);
+	//}
 
-	private void OnRequestPrefab(NetworkMessage netMsg)
-	{
-		MsgTypes.PlayerPrefabMsg msg = new MsgTypes.PlayerPrefabMsg();
-		msg.controllerID = netMsg.ReadMessage<MsgTypes.PlayerPrefabMsg>().controllerID;
-		msg.prefabIndex = playerPrefabIndex;
-		client.Send(MsgTypes.PlayerPrefab, msg);
-	}
+	//private void OnRequestPrefab(NetworkMessage netMsg)
+	//{
+	//	MsgTypes.PlayerPrefabMsg msg = new MsgTypes.PlayerPrefabMsg();
+	//	msg.controllerID = netMsg.ReadMessage<MsgTypes.PlayerPrefabMsg>().controllerID;
+	//	msg.prefabIndex = playerPrefabIndex;
+	//	client.Send(MsgTypes.PlayerPrefab, msg);
+	//}
 
-	private void OnResponsePrefab(NetworkMessage netMsg)
-	{
-		MsgTypes.PlayerPrefabMsg msg = netMsg.ReadMessage<MsgTypes.PlayerPrefabMsg>();
-		playerPrefab = spawnPrefabs[msg.prefabIndex];
-		base.OnServerAddPlayer(netMsg.conn, msg.controllerID);
-		Debug.Log(playerPrefab.name + " spawned!");
-	}
+	//private void OnResponsePrefab(NetworkMessage netMsg)
+	//{
+	//	MsgTypes.PlayerPrefabMsg msg = netMsg.ReadMessage<MsgTypes.PlayerPrefabMsg>();
+	//	playerPrefab = spawnPrefabs[msg.prefabIndex];
+	//	base.OnServerAddPlayer(netMsg.conn, msg.controllerID);
+	//	Debug.Log(playerPrefab.name + " spawned!");
+	//}
 
-	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
-	{
-		MsgTypes.PlayerPrefabMsg msg = new MsgTypes.PlayerPrefabMsg();
-		msg.controllerID = playerControllerId;
-		NetworkServer.SendToClient(conn.connectionId, MsgTypes.PlayerPrefab, msg);
-	}
+	//public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+	//{
+	//	MsgTypes.PlayerPrefabMsg msg = new MsgTypes.PlayerPrefabMsg();
+	//	msg.controllerID = playerControllerId;
+	//	NetworkServer.SendToClient(conn.connectionId, MsgTypes.PlayerPrefab, msg);
+	//}
 
-	// I have put a toggle UI on gameObjects called PC1 and PC2 to select two different character types.
-	// on toggle, this function is called, which updates the playerPrefabIndex
-	// The index will be the number from the registered spawnable prefabs that 
-	// you want for your player
-	public void UpdatePC()
-	{
-		if (GameObject.Find("PC1").GetComponent<Toggle>().isOn)
-		{
-			playerPrefabIndex = 0;
-		}
-		else if (GameObject.Find("PC2").GetComponent<Toggle>().isOn)
-		{
-			playerPrefabIndex = 1;
-		}
-	}
+	//// I have put a toggle UI on gameObjects called PC1 and PC2 to select two different character types.
+	//// on toggle, this function is called, which updates the playerPrefabIndex
+	//// The index will be the number from the registered spawnable prefabs that 
+	//// you want for your player
+	//public void UpdatePC()
+	//{
+	//	if (GameObject.Find("PC1").GetComponent<Toggle>().isOn)
+	//	{
+	//		playerPrefabIndex = 0;
+	//	}
+	//	else if (GameObject.Find("PC2").GetComponent<Toggle>().isOn)
+	//	{
+	//		playerPrefabIndex = 1;
+	//	}
+	//}
 
 	void Start()
 	{
