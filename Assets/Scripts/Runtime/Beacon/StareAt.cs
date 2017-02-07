ed697345029d46418a7303411bb70ff3106e2469
Dedie Kanda <dolente@live.com>
2017-02-06 23:15:26 -0500

    HeighScale script added

---
+++ Assets/Scripts/Runtime/Beacon/StareAt.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StareAt : MonoBehaviour {
+
+	public Transform a_Beacon;
+	public Transform p_Camera;
+
+	public bool lookAtCamera;
+	public bool lookAtBecon;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (lookAtCamera) { 	
+			transform.LookAt (transform.position + p_Camera.transform.rotation * Vector3.forward, p_Camera.transform.rotation * Vector3.up);
+		}
+
+		if (lookAtBecon) {
+			transform.LookAt (a_Beacon.position);
+		}
+	}
+}
