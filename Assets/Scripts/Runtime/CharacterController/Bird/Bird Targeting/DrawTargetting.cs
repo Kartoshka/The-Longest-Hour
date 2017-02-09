8ee029224e4dd89a05318813612e329e65959d29
Monica Pabin <monicapabin@gmail.com>
2017-02-09 14:10:28 -0500

    respawns you if you fall off the map

--- Assets/Scripts/Runtime/CharacterController/Bird/Bird Targeting/DrawTargetting.cs
+++ Assets/Scripts/Runtime/CharacterController/Bird/Bird Targeting/DrawTargetting.cs
@@ -16,7 +16,6 @@ public class DrawTargetting : AbAirTargeting {
 	protected override void OnEnableTargeting()
 	{
         m_lineDrawer = gameObject.GetComponent<LineDrawerComponent>();
-        m_lineDrawer.reset();
 	}
 
 	protected override void OnTriggerTargeting ()
---
+++ Assets/Scripts/Runtime/RespawnOnFall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnOnFall : MonoBehaviour {
+    Vector3 m_initialPosition = new Vector3(0,0,0);
+
+	// Use this for initialization
+	void Start () {
+        m_initialPosition = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag.Equals("FallRespawner"))
+        {
+            gameObject.transform.position = m_initialPosition;
+        }
+    }
+}
