8a1635cf649db70e49e86c4ff558237501a5221e
gurshane <gurshane.sidhu@mail.mcgill.ca>
2017-02-08 20:01:43 -0500

    Grenade tagging and single tagging with deactivation

---
+++ Assets/Scripts/Runtime/GroundTagFlashing.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundTagFlashing : MonoBehaviour
+{
+
+    public float lowVal;
+    public float highVal;
+    public float stepAmount;
+
+    private MeshRenderer meshRenderer;
+    private float targetVal;
+    private bool makeBrighter;
+
+    // Use this for initialization
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        targetVal = highVal;
+        makeBrighter = true;
+    }
+
+    void Update()
+    {
+        float currentEdging = meshRenderer.material.GetFloat("_Edging");
+        if (makeBrighter)
+        {
+            float newVal = currentEdging + (stepAmount * Time.deltaTime);
+            meshRenderer.material.SetFloat("_Edging", newVal);
+            if (newVal > targetVal)
+            {
+                makeBrighter = false;
+                targetVal = lowVal;
+            }
+        }
+        else
+        {
+            float newVal = currentEdging - (stepAmount * Time.deltaTime);
+            meshRenderer.material.SetFloat("_Edging", newVal);
+            if (newVal < targetVal)
+            {
+                makeBrighter = true;
+                targetVal = highVal;
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Runtime/TagSingle.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagSingle : MonoBehaviour
+{
+    
+    bool isInTaggableTrigger;
+    GameObject tagTarget;
+
+	// Use this for initialization
+	void Start ()
+    {
+        isInTaggableTrigger = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetButtonDown("Fire1"))
+        {
+            if(isInTaggableTrigger)
+            {
+                taggify();
+            }
+        }
+	}
+
+   
+
+    void taggify()
+    {
+        if(tagTarget.GetComponent<TimeControllable>().enabled)
+        {
+            unTaggify();
+            return;
+        }
+        tagTarget.GetComponent<TimeControllable>().enabled = true;
+        tagTarget.GetComponent<TimeControllable>().Activate();
+    }
+
+    void unTaggify()
+    {
+        tagTarget.GetComponent<TimeControllable>().Deactivate();
+        tagTarget.GetComponent<TimeControllable>().enabled = false;
+    }
+}
