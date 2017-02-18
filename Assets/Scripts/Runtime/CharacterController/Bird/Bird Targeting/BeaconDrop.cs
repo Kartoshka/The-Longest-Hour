9b965b86fa7532d9d6a1f738d494b56d5953cf31
Kartoshka <eli.harfouche@gmail.com>
2017-02-18 12:25:49 -0500

    Implement bird beacon drop

---
+++ Assets/Scripts/Runtime/CharacterController/Bird/Bird Targeting/BeaconDrop.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeaconDrop : SelectableAbility {
+
+	public GameObject beaconPrefab;
+	private GameObject currentBeacon;
+
+	protected override void OnEnableTargeting ()
+	{
+		
+	}
+
+	protected override void OnActivate ()
+	{
+		RaycastHit result;
+		if(findTarget(out result) && beaconPrefab!=null){
+			if (currentBeacon != null) {
+				Destroy (currentBeacon);
+			}
+			currentBeacon = (GameObject)Instantiate (beaconPrefab, result.point, Quaternion.identity);
+		}
+	}
+
+	protected override void OnDisactivate ()
+	{
+	}
+
+	protected override void OnDisableTargeting ()
+	{
+
+	}
+}
