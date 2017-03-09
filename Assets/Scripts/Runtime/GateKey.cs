7a2f769e8906299b8f220b1987be97d2425d44f5
gurshane <gurshane.sidhu@mail.mcgill.ca>
2017-03-09 00:00:24 -0500

    Added gate puzzle to threePuzzleLevel

---
+++ Assets/Scripts/Runtime/GateKey.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateKey : MonoBehaviour {
+
+    public int key;
+
+    GateState gateState;
+
+    void Start()
+    {
+        gateState = GetComponentInParent<GateState>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag.Equals("Player"))
+        {
+            gateState.disableLock(key);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/GateState.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateState : MonoBehaviour {
+
+    public GameObject redKey;
+    public GameObject blueKey;
+    public GameObject greenKey;
+    public GameObject yellowKey;
+
+    int numLocks;
+
+	// Use this for initialization
+	void Start ()
+    {
+        numLocks = 4;	
+	}
+
+    public void disableLock(int key)
+    {
+        numLocks--;
+
+        switch (key)
+        {
+            case (0):
+                Destroy(redKey);
+                break;
+            case (1):
+                Destroy(blueKey);
+                break;
+            case (2):
+                Destroy(greenKey);
+                break;
+            case (3):
+                Destroy(yellowKey);
+                break;
+        }
+
+        if(numLocks == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
