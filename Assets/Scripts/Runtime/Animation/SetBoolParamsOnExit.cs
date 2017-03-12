24b635aa0c8c1fbb72047b5b3771529f68c50dea
Kartoshka <eli.harfouche@gmail.com>
2017-03-11 22:47:39 -0500

    Fix bug where bear could move while hitting stuff/sleeping/not idle

---
+++ Assets/Scripts/Runtime/Animation/SetBoolParamsOnExit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetBoolParamsOnExit : StateMachineBehaviour {
+
+
+	public List<string> m_onParams;
+	public List<string> m_offParams;
+
+	//OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		foreach(string param in m_onParams)
+		{
+			animator.SetBool(param, true);
+		}
+		foreach (string param in m_offParams)
+		{
+			animator.SetBool(param, false);
+		}
+	}
+}
