149ec30db1e4a3476b6ca0ffd72cb356380ce8a6
gurshane <gurshane.sidhu@mail.mcgill.ca>
2017-03-16 11:31:00 -0400

    generator behaviour

---
+++ Assets/CameraEffects.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraEffects : MonoBehaviour {
+
+    Camera main;
+
+    void Start()
+    {
+        main = Camera.main;
+        main.GetComponent<Twirl>();
+    }
+
+	void fadeIn()
+    {
+
+    }
+
+    void fadeOut()
+    {
+
+    }
+}
