94241600168bda46c78071902c0332fcf2965a76
Kartoshka <eli.harfouche@gmail.com>
2017-03-17 08:57:46 -0400

    Implement component which matches forward vector of object to that of camera, freezing y.

---
+++ Assets/MatchCameraForward.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchCameraForward : MonoBehaviour {
+
+	private Transform camTransform;
+	// Use this for initialization
+	void Start () {
+		GameObject cam = GameObject.FindGameObjectWithTag ("MainCamera");
+		if (cam)
+		{
+			camTransform = cam.transform;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (camTransform)
+		{
+			this.gameObject.transform.forward = new Vector3 (camTransform.forward.x, 0, camTransform.forward.z).normalized;
+		}
+	}
+}
