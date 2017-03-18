1e7e19b460ee55297af583e0e43349c0ffb55f7c
Kartoshka <eli.harfouche@gmail.com>
2017-03-17 21:12:34 -0400

    Add option to not update certain axis for forward matching

--- Assets/MatchCameraForward.cs
+++ Assets/MatchCameraForward.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class MatchCameraForward : MonoBehaviour {
 
 	private Transform camTransform;
+	public bool freezeX;
+	public bool freezeY;
+	public bool freezeZ;
 	// Use this for initialization
 	void Start () {
 		GameObject cam = GameObject.FindGameObjectWithTag ("MainCamera");
@@ -18,7 +21,7 @@ public class MatchCameraForward : MonoBehaviour {
 	void Update () {
 		if (camTransform)
 		{
-			this.gameObject.transform.forward = new Vector3 (camTransform.forward.x, 0, camTransform.forward.z).normalized;
+			this.gameObject.transform.forward = new Vector3 (freezeX?this.transform.forward.x:camTransform.forward.x, freezeY?this.transform.forward.y:camTransform.forward.y, freezeZ?this.transform.forward.z:camTransform.forward.z).normalized;
 		}
 	}
 }
--- Assets/UpdateForward.cs
+++ Assets/UpdateForward.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class UpdateForward : MonoBehaviour {
 
 	public float threshold = 0.1f;
+	public bool freezeX, freezeY, freezeZ;
 	private Vector3 lastPos;
 	// Use this for initialization
 	void Start () {
@@ -16,7 +17,7 @@ public class UpdateForward : MonoBehaviour {
 		Vector3 movDifference = this.transform.position - lastPos;
 		if (movDifference.magnitude > threshold)
 		{
-			this.gameObject.transform.forward = new Vector3 (movDifference.x, 0, movDifference.z).normalized;
+			this.gameObject.transform.forward = new Vector3 (freezeX?this.transform.forward.x:movDifference.x, freezeY?this.transform.forward.y:movDifference.y, freezeZ?this.transform.forward.z:movDifference.z).normalized;
 		}
 		lastPos = this.transform.position;
 		
