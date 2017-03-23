616455d6a5b5f8bff0e7b9d966a1535dfe7258fa
Kartoshka <eli.harfouche@gmail.com>
2017-03-23 00:02:22 -0400

    network camera controller

---
+++ Assets/NetworkedCameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NetworkedCameraController :  NetworkBehaviour{
+
+	public CameraController m_camController;
+	// Use this for initialization
+	void Start () {
+		if (m_camController)
+		{
+			m_camController.gameObject.SetActive (this.hasAuthority);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
