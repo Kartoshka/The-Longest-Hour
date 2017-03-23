198c84d69df1d28d8cfe6ecbcc3dace0c4c8036c
gurshane <gurshane.sidhu@mail.mcgill.ca>
2017-03-23 05:27:39 -0400

    spawn generator ends

--- Assets/NetworkSpawnSpecific.cs
+++ Assets/NetworkSpawnSpecific.cs
@@ -14,11 +14,22 @@ public class NetworkSpawnSpecific : NetworkBehaviour {
     public int gateNetworkIndex = 3;
     public Transform m_gateSpawn;
 
+    public Transform m_l1Spawn;
+    public Transform m_l2Spawn;
+    public Transform m_ll1Spawn;
+    public Transform m_ll2Spawn;
+    public Transform m_ll3Spawn;
+    public Transform m_ll4Spawn;
+
     private void spawnEntities(int networkPrefabIndex, List<Transform> spawnLocations)
     {
         for (int i = 0; i < spawnLocations.Count; ++i)
         {
             Transform transform = spawnLocations[i];
+            if(transform == null)
+            {
+                continue;
+            }
             GameObject gameObject = GameObject.Instantiate(NetworkLobbyManager.singleton.spawnPrefabs[networkPrefabIndex], transform.position, transform.rotation, this.transform);
             NetworkServer.Spawn(gameObject);
         }
@@ -26,6 +37,10 @@ public class NetworkSpawnSpecific : NetworkBehaviour {
 
     private void spawnEntity(int networkPrefabIndex, Transform spawnLocation)
     {
+        if(spawnLocation == null)
+        {
+            return;
+        }
         Transform transform = spawnLocation;
         GameObject gameObject = GameObject.Instantiate(NetworkLobbyManager.singleton.spawnPrefabs[networkPrefabIndex], transform.position, transform.rotation, this.transform);
         NetworkServer.Spawn(gameObject);
@@ -39,7 +54,14 @@ public class NetworkSpawnSpecific : NetworkBehaviour {
             spawnEntity(blockSpawnNetworkIndex, m_blockSpawn);
             spawnEntity(gateNetworkIndex, m_gateSpawn);
             spawnEntities(ballSpawnNetworkIndex, m_ballSpawns);
+            spawnEntity(11, m_l1Spawn);
+            spawnEntity(12, m_l2Spawn);
+            spawnEntity(13, m_ll1Spawn);
+            spawnEntity(14, m_ll2Spawn);
+            spawnEntity(15, m_ll3Spawn);
+            spawnEntity(16, m_ll4Spawn);
         }
     }
     
+
 }
